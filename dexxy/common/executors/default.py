da95e6002b4a9814f785062c4d7d69d08ac8a059
Dale <campb223@go.stockton.edu>
2022-11-08 02:04:19 -0500

    starting default.py

---
+++ dexxy/common/executors/default.py
@@ -0,0 +1,42 @@
+#from dexxy.common.executors.base import BaseExecutor
+#from dexxy.common.logger import LoggingStuff
+from dexxy.common.clients.utils import getTaskResult
+from typing import TypeVar
+
+Queue = TypeVar('Queue')
+
+#class DefaultWorker(LoggingStuff)
+class DefaultWorker():
+    workerID = 0
+    
+    def __init__(self, taskQueue: Queue, resultQueue: Queue):
+        DefaultWorker.workerID += 1
+        self.taskQueue = taskQueue
+        self.resultQueue = resultQueue
+        self._log = self.logger
+        
+    def run(self):
+        
+        while not self.taskQueue.empty():
+            
+            _task = self.taskQueue.get()
+            _task.updateStatus('Running')
+            self._log.info('Running Tasks %s on Worker %s ' % (_task.name, self.workerID))
+            
+            if _task.dependsOn:
+                inputs = ()
+                for depTask in list(dict.fromkeys(_task.dependsOn).keys()):
+                    for completedTask in list(self.resultQueue.queue):
+                        if depTask.tid == completedTask.tid:
+                            inputData = getTaskResult(completedTask)
+                            inputs = inputs + inputData
+            else:
+                inputs = tuple()
+                
+            _task.run(inputs)
+            _task.updateStatus('Completed')
+            
+            self.resultQueue.put(_task)
+            
+            self.taskQueue.task_done()
+            
