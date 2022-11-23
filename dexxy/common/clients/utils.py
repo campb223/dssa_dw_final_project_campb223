6eaecb501587bc549c5d61a52d72f6cb62aaf5ca
Dale <campb223@go.stockton.edu>
2022-11-23 00:00:22 -0500

    Updated utils

--- dexxy/common/clients/utils.py
+++ dexxy/common/clients/utils.py
@@ -2,13 +2,20 @@ from typing import Tuple, Any
 from uuid import uuid4, uuid5, NAMESPACE_OID
 
 def generateUniqueID(name: str = None) -> str:
-    # Comments in Tasks 32:19 into video
+    """
+    Generates a unique ID
+    """
     
+    # If the name is provided, generate a UUID based on the SHA-1 hash of a namespace identifier (which is a UUID) and a name (which is a string).
     if name:
         return str(uuid5(NAMESPACE_OID, name))
+    # Otherwise generate a random UUID.
     return str(uuid4())
 
 def getTaskResult(task) -> Tuple[Any]:
+    """
+    Takes in a node in the Task with a list of UUIDs to lookup. Then looks up the data required to run a task using the provided list of UUIDs. 
+    """
     
     inputs = []
     if task is not None:
