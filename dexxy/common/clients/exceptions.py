aebbe95bede2720ad553671500ca8e0fd6ecdbeb
Dale <campb223@go.stockton.edu>
2022-11-22 23:54:24 -0500

    Updated Exceptions & Task imports

--- dexxy/common/clients/exceptions.py
+++ dexxy/common/clients/exceptions.py
@@ -1,8 +1,25 @@
-import exceptions
+# Defining some exceptions we could expect to see during runtime.
 
 class CompatibilityException(Exception):
     pass
 
-
 class MissingTypeHintException(Exception):
+    pass
+
+class OutputNotFoundError(Exception):
+    pass
+
+class CircularDependencyError(Exception):
+    pass
+
+class MissingDependencyError(Exception):
+    pass
+
+class DependencyError(Exception):
+    pass
+
+class ActivityFailedError(Exception):
+    pass
+
+class NotFoundError(Exception):
     pass
--- dexxy/common/clients/tasks.py
+++ dexxy/common/clients/tasks.py
@@ -1,6 +1,6 @@
 from typing import List, Union, TypeVar, Any, Callable, Literal
 from inspect import signature
-from dexxy.common.clients.logger import LoggingMsg
+from logger import LoggingStuff
 from exceptions import CompatibilityException, MissingTypeHintException
 from utils import generateUniqueID
 from abc import ABCMeta, abstractclassmethod
